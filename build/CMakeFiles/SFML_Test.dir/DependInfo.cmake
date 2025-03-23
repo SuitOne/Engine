3589c4e522ba5e35ba14f2990f968e0a12dc5dc5
Devante Coley <devantecoley802@gmail.com>
2025-03-22 22:51:01 -0400

    TODO >> Test for windows compatibility on cmake! Cleaned up the project. Build files should go in build directory and be added to a .gitignore!

---
+++ build/CMakeFiles/SFML_Test.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/gamessd/Engine/src/engine/engine.cpp" "CMakeFiles/SFML_Test.dir/src/engine/engine.cpp.o" "gcc" "CMakeFiles/SFML_Test.dir/src/engine/engine.cpp.o.d"
+  "/mnt/gamessd/Engine/src/engine/entity.cpp" "CMakeFiles/SFML_Test.dir/src/engine/entity.cpp.o" "gcc" "CMakeFiles/SFML_Test.dir/src/engine/entity.cpp.o.d"
+  "/mnt/gamessd/Engine/src/engine/input.cpp" "CMakeFiles/SFML_Test.dir/src/engine/input.cpp.o" "gcc" "CMakeFiles/SFML_Test.dir/src/engine/input.cpp.o.d"
+  "/mnt/gamessd/Engine/src/engine/renderer.cpp" "CMakeFiles/SFML_Test.dir/src/engine/renderer.cpp.o" "gcc" "CMakeFiles/SFML_Test.dir/src/engine/renderer.cpp.o.d"
+  "/mnt/gamessd/Engine/src/engine/scene.cpp" "CMakeFiles/SFML_Test.dir/src/engine/scene.cpp.o" "gcc" "CMakeFiles/SFML_Test.dir/src/engine/scene.cpp.o.d"
+  "/mnt/gamessd/Engine/src/engine/sceneManager.cpp" "CMakeFiles/SFML_Test.dir/src/engine/sceneManager.cpp.o" "gcc" "CMakeFiles/SFML_Test.dir/src/engine/sceneManager.cpp.o.d"
+  "/mnt/gamessd/Engine/src/game/components/sprite.cpp" "CMakeFiles/SFML_Test.dir/src/game/components/sprite.cpp.o" "gcc" "CMakeFiles/SFML_Test.dir/src/game/components/sprite.cpp.o.d"
+  "/mnt/gamessd/Engine/src/game/scenes/sMainMenu.cpp" "CMakeFiles/SFML_Test.dir/src/game/scenes/sMainMenu.cpp.o" "gcc" "CMakeFiles/SFML_Test.dir/src/game/scenes/sMainMenu.cpp.o.d"
+  "/mnt/gamessd/Engine/src/game/scenes/sMainTest.cpp" "CMakeFiles/SFML_Test.dir/src/game/scenes/sMainTest.cpp.o" "gcc" "CMakeFiles/SFML_Test.dir/src/game/scenes/sMainTest.cpp.o.d"
+  "/mnt/gamessd/Engine/src/main.cpp" "CMakeFiles/SFML_Test.dir/src/main.cpp.o" "gcc" "CMakeFiles/SFML_Test.dir/src/main.cpp.o.d"
+  "" "SFML_Test" "gcc" "CMakeFiles/SFML_Test.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/SFML_Test.dir/cmake_clean.cmake
@@ -0,0 +1,30 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SFML_Test.dir/link.d"
+  "CMakeFiles/SFML_Test.dir/src/engine/engine.cpp.o"
+  "CMakeFiles/SFML_Test.dir/src/engine/engine.cpp.o.d"
+  "CMakeFiles/SFML_Test.dir/src/engine/entity.cpp.o"
+  "CMakeFiles/SFML_Test.dir/src/engine/entity.cpp.o.d"
+  "CMakeFiles/SFML_Test.dir/src/engine/input.cpp.o"
+  "CMakeFiles/SFML_Test.dir/src/engine/input.cpp.o.d"
+  "CMakeFiles/SFML_Test.dir/src/engine/renderer.cpp.o"
+  "CMakeFiles/SFML_Test.dir/src/engine/renderer.cpp.o.d"
+  "CMakeFiles/SFML_Test.dir/src/engine/scene.cpp.o"
+  "CMakeFiles/SFML_Test.dir/src/engine/scene.cpp.o.d"
+  "CMakeFiles/SFML_Test.dir/src/engine/sceneManager.cpp.o"
+  "CMakeFiles/SFML_Test.dir/src/engine/sceneManager.cpp.o.d"
+  "CMakeFiles/SFML_Test.dir/src/game/components/sprite.cpp.o"
+  "CMakeFiles/SFML_Test.dir/src/game/components/sprite.cpp.o.d"
+  "CMakeFiles/SFML_Test.dir/src/game/scenes/sMainMenu.cpp.o"
+  "CMakeFiles/SFML_Test.dir/src/game/scenes/sMainMenu.cpp.o.d"
+  "CMakeFiles/SFML_Test.dir/src/game/scenes/sMainTest.cpp.o"
+  "CMakeFiles/SFML_Test.dir/src/game/scenes/sMainTest.cpp.o.d"
+  "CMakeFiles/SFML_Test.dir/src/main.cpp.o"
+  "CMakeFiles/SFML_Test.dir/src/main.cpp.o.d"
+  "SFML_Test"
+  "SFML_Test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SFML_Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
