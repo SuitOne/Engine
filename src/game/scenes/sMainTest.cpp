bbf523bda2e31e581d53d3ba1f0052002c8aa7b0
SuitOne <95261806+SuitOne@users.noreply.github.com>
2025-03-25 13:09:20 -0500

    Removed logic from scene structure

--- src/game/scenes/sMainTest.cpp
+++ src/game/scenes/sMainTest.cpp
@@ -1,5 +1,6 @@
 #include "game/scenes/sMainTest.hpp"
 #include "game/components/sprite.hpp"
+#include "game/components/voronoidiagram.hpp"
 
 #include "engine/engine.hpp"
 #include "engine/renderer.hpp"
@@ -9,34 +10,13 @@
 
 sMainTest::sMainTest(Engine& engine) :
     Scene(engine) {
-        
-    // Create player
-    player = std::make_shared<sf::CircleShape>(50.f);
-    player->setFillColor(sf::Color::Blue);
-    player->setOutlineThickness(5.f);
-    player->setOutlineColor(sf::Color::Cyan);
-    std::shared_ptr<Entity> playerEntity = createEntity();
-    std::shared_ptr<Sprite> playerSprite = std::make_shared<Sprite>(playerEntity, player);
-    playerEntity->addComponent(playerSprite);
+
+    // Create voronoi
+    std::shared_ptr<Entity> voronoiEntity = createEntity();
+    voronoiEntity->addComponent(std::make_shared<VoronoiDiagram>(engine));
 }
 
 void sMainTest::tick(){
     // Call base
     Scene::tick();
-
-    // Movement test
-    float speed = engine.eInput->getKey(sf::Keyboard::Scancode::LShift) ? 5 : 2.5f;
-
-    if(engine.eInput->getKey(sf::Keyboard::Scancode::S)){
-        player->move({0.f, speed});
-    }
-    if(engine.eInput->getKey(sf::Keyboard::Scancode::W)){
-        player->move({0.f, -speed});
-    }
-    if(engine.eInput->getKey(sf::Keyboard::Scancode::A)){
-        player->move({-speed, 0});
-    }
-    if(engine.eInput->getKey(sf::Keyboard::Scancode::D)){
-        player->move({speed, 0});
-    }
 }
