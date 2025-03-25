5a9ced235c56e0d58e1803039f45503e32b46e67
SuitOne <95261806+SuitOne@users.noreply.github.com>
2025-03-25 13:06:59 -0500

    Added primitives rendering capability to Renderer

--- src/engine/renderer.cpp
+++ src/engine/renderer.cpp
@@ -11,6 +11,10 @@ Renderer::Renderer(sf::RenderWindow& renderWindow)
 void Renderer::addDrawable(std::shared_ptr<sf::Drawable> drawable){
     drawables.push_back(drawable);
 }
+void Renderer::addDrawable(std::vector<std::shared_ptr<sf::Vertex>> vertices, sf::PrimitiveType primType) {
+    prims.push_back(Prim(vertices, primType));
+    std::cout << "?\n";
+}
 
 void Renderer::clearDrawables(){
     drawables.clear();
@@ -20,9 +24,23 @@ void Renderer::drawFrame(){
         // Clear window
         Renderer::renderWindow.clear(sf::Color::Black);
 
-        // Draw
+        // Draw drawables
         for(const auto& drawable : drawables){
             renderWindow.draw(*drawable);
+        }
+
+        // Draw primitives
+        for(const auto& primitive : prims){
+            std::vector<sf::Vertex> rawVerts;
+            rawVerts.reserve(primitive.verts.size());
+            
+            for(const auto& vertexPtr : primitive.verts) {
+                if (vertexPtr) {
+                    rawVerts.push_back(*vertexPtr);
+                }
+            }
+
+            renderWindow.draw(rawVerts.data(), rawVerts.size(), primitive.primType);
         }
 
         // End render frame
