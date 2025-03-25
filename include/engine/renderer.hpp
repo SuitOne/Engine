71aac31f3c3ecf109235425817d0451e2929619a
SuitOne <95261806+SuitOne@users.noreply.github.com>
2025-03-25 13:07:20 -0500

    Update renderer.hpp

--- include/engine/renderer.hpp
+++ include/engine/renderer.hpp
@@ -4,14 +4,24 @@
 #include <memory>
 
 class Renderer{
+    struct Prim {
+        const std::vector<std::shared_ptr<sf::Vertex>> verts;
+        const sf::PrimitiveType primType;
+
+        Prim(std::vector<std::shared_ptr<sf::Vertex>> verts, sf::PrimitiveType primType) 
+        : verts(verts), primType(primType) {}
+    };
+
     private:
         sf::RenderWindow& renderWindow;
         std::vector<std::shared_ptr<sf::Drawable>> drawables;
+        std::vector<Prim> prims;
 
     public:
         Renderer(sf::RenderWindow& renderWindow);
 
         void addDrawable(std::shared_ptr<sf::Drawable> drawable);
+        void addDrawable(std::vector<std::shared_ptr<sf::Vertex>> vertices, sf::PrimitiveType primType);
         void clearDrawables();
         void drawFrame();
 };
