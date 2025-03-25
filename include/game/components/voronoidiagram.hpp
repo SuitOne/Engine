a8d1dadc3ea4153e25a362b326ce9df483371504
SuitOne <95261806+SuitOne@users.noreply.github.com>
2025-03-25 13:07:54 -0500

    Create voronoidiagram.hpp

---
+++ include/game/components/voronoidiagram.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <vector>
+#include "boost/polygon/voronoi.hpp"
+#include "sfml_includes.hpp"
+#include "engine/component.hpp"
+
+class Engine;
+
+class VoronoiDiagram : public Component {
+    public:
+        VoronoiDiagram(Engine& engineRef);
+
+        void generate(const std::vector<sf::Vector2f>& points);
+
+        void init() override;
+        void tick() override;
+
+    private:
+        boost::polygon::voronoi_diagram<double> vd;
+        std::vector<sf::Vector2f> inputPoints;
+        Engine& engine;
+
+        void draw() const;
+        void drawEdge(const boost::polygon::voronoi_edge<double> edge) const;
+};
