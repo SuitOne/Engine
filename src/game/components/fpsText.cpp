ff808ef317b09aff9c50fc7a7f5702ed7b24af97
SuitOne <95261806+SuitOne@users.noreply.github.com>
2025-03-26 10:17:31 -0500

    Added screen lock

--- src/game/components/fpsText.cpp
+++ src/game/components/fpsText.cpp
@@ -17,5 +17,11 @@ void FPSText::init() {
 
 void FPSText::tick() {
     text.setString(std::to_string(pEngine->fps));
+
+    sf::Vector2f viewCenter = pEngine->renderWindow.getView().getCenter();
+    sf::Vector2f viewSize = pEngine->renderWindow.getView().getSize();
+    
+    text.setPosition(viewCenter + sf::Vector2f{viewSize.x / -2 + 10, viewSize.y / -2 + 10});
+
     pEngine->eRenderer->addDrawable(std::make_shared<sf::Text>(text));
 }
