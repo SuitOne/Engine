f5ff14f9c8820961001366150e0139e56e65ea42
SuitOne <95261806+SuitOne@users.noreply.github.com>
2025-03-27 16:24:56 -0500

    Finished voronoi concept

--- src/game/components/voronoidiagram.cpp
+++ src/game/components/voronoidiagram.cpp
@@ -11,14 +11,14 @@ using boost::polygon::point_data;
 VoronoiDiagram::VoronoiDiagram(Engine& engineRef) : engine(engineRef){}
 
 void VoronoiDiagram::init(){
-    generate(generatePoints(50, 2000));
+    generate(generatePoints(200, 2000));
 }
 
 void VoronoiDiagram::tick(){
     timer += engine.deltaTime.asSeconds();
 
     if(timer >= 2){
-        generate(generatePoints(100, 2000));
+        generate(generatePoints(200, 2000));
         timer = 0;
     }
 
@@ -40,63 +40,129 @@ void VoronoiDiagram::generate(const std::vector<sf::Vector2f>& points){
 
 void VoronoiDiagram::draw() const{
     // Forward input points to renderer
-    for (const auto& p : inputPoints) {
-        sf::CircleShape shape(3.0f);
-        shape.setFillColor(sf::Color::Red);
-        shape.setPosition({p.x - 3.0f, p.y - 3.0f});
+    // Ignore for now, mainly for debugging cell position
+    // for (const auto& p : inputPoints) {
+    //     sf::CircleShape shape(3.0f);
+    //     shape.setFillColor(sf::Color::Red);
+    //     shape.setPosition({p.x - 3.0f, p.y - 3.0f});
 
-        engine.eRenderer->addRenderCommand(std::make_shared<sf::CircleShape>(shape));
-    }
+    //     engine.eRenderer->addRenderCommand(std::make_shared<sf::CircleShape>(shape), 101);
+    // }
 
+    // Draw cells
     for(const auto& cell : vd.cells()){
         drawCell(cell);
     }
+
+    // Draw bounding box
+    sf::Vector2f tRight {boundingBox.x, boundingBox.y};
+    sf::Vector2f bRight {boundingBox.x, -boundingBox.y};
+    sf::Vector2f bLeft {-boundingBox.x, -boundingBox.y};
+    sf::Vector2f tLeft {-boundingBox.x, boundingBox.y};
+
+    drawEdge(tLeft, tRight, sf::Color::Red);
+    drawEdge(tRight, bRight, sf::Color::Red);
+    drawEdge(bRight, bLeft, sf::Color::Red);
+    drawEdge(bLeft, tLeft, sf::Color::Red);
+
+    // Draw background
+    sf::RectangleShape backdrop;
+    backdrop.setSize(sf::Vector2f(boundingBox.x * 2, boundingBox.y * 2));
+    backdrop.setPosition(sf::Vector2f(-boundingBox.x, -boundingBox.y));
+    backdrop.setFillColor(sf::Color::Blue);
+    engine.eRenderer->addRenderCommand(std::make_shared<sf::RectangleShape>(backdrop), 90);
 }
 
 void VoronoiDiagram::drawCell(const boost::polygon::voronoi_cell<double>& cell) const{
-    // Create vector for vertex storage
-    std::vector<const boost::polygon::voronoi_vertex<double>*> vertVect {};
-    std::vector<std::shared_ptr<sf::Vertex>> vectorVector {};
+    // Create vector for edge, vertex and vector storage
+    std::vector<const boost::polygon::voronoi_edge<double>*> edges {};
+    std::vector<const boost::polygon::voronoi_vertex<double>*> cellVertices {};
+    std::vector<std::shared_ptr<sf::Vertex>> fanVertices {};
 
     // Get site point of cell
     std::size_t index = cell.source_index();
     auto point = inputPoints[index];
 
     // Push back the site point
-    vectorVector.push_back(std::make_shared<sf::Vertex>(point));
+    fanVertices.push_back(std::make_shared<sf::Vertex>(point, sf::Color(20, 125, 20)));
 
     // Get first cell edge
     const boost::polygon::voronoi_edge<double>* edge = 
         cell.incident_edge();
 
-    // Loop over all cell edges
+    // Collect all valid edges and vertices
     do {
-        // Draw the edge
-        drawEdge(*edge);
+        // Abort cell if infinite edges
+        if(edge->is_infinite()) return;
+
+        // Store vertices, abort if outside bounds
+        auto vert0 = edge->vertex0();
+        auto vert1 = edge->vertex1();
+
+        // Greater check
+        if(vert0->x() > boundingBox.x ||
+         vert0->y() > boundingBox.y ||
+         vert1->x() > boundingBox.x ||
+         vert1->y() > boundingBox.y) return;
+
+        // Lesser check
+        if(vert0->x() < -boundingBox.x ||
+         vert0->y() < -boundingBox.y ||
+         vert1->x() < -boundingBox.x ||
+         vert1->y() < -boundingBox.y) return;
+
+        // Push back edge
+        edges.push_back(edge);
         
         // Add vertices to vector
-        vertVect.push_back(edge->vertex0());
-        vertVect.push_back(edge->vertex1());
+        cellVertices.push_back(edge->vertex0());
+        cellVertices.push_back(edge->vertex1());
 
         // Move to next edge
         edge = edge->next();
     } while (edge != cell.incident_edge());
 
-    // Convert collected vertices to sf vectors
-    for(const auto& vert : vertVect){
-        if(vert != nullptr){
-            sf::Vector2f newVect = sf::Vector2f(static_cast<float>(vert->x()), static_cast<float>(vert->y()));
-            sf::Vertex newVert = sf::Vertex(newVect, sf::Color::Blue);
-            vectorVector.push_back(std::make_shared<sf::Vertex>(newVert));
-        }
-        
+    // Calculate the distance of the point from the center
+    sf::Vector2f center(0, 0);
+    double distance = std::sqrt(std::pow(point.x - center.x, 2) + std::pow(point.y - center.y, 2));
+    double maxDistance = std::sqrt(2 * std::pow(boundingBox.x / 2.0f, 2));
+
+    // Determine cell color based on distance
+    sf::Color cellColor;
+
+    if (distance < maxDistance / 3) {
+        // Closer to the center
+        cellColor = sf::Color(50, 50, 50);
+        fanVertices.front()->color = sf::Color(150, 150, 150);
+    } else if (distance < 2.75f * maxDistance / 3) {
+        // Middle
+        cellColor = sf::Color(50, 120, 50);
+        fanVertices.front()->color = sf::Color(70, 140, 70);
+    } else {
+        // Closer to the edge
+        cellColor = sf::Color(210, 180, 140);
+        fanVertices.front()->color = sf::Color(230, 210, 160);
     }
 
-    // Create primitive
-    engine.eRenderer->addRenderCommand(vectorVector, sf::PrimitiveType::TriangleFan);
+    // Convert collected vertices to sf vectors
+    for(const auto& vert : cellVertices){
+        if(vert != nullptr){
+            sf::Vector2f newVect = sf::Vector2f(static_cast<float>(vert->x()), static_cast<float>(vert->y()));
+            sf::Vertex newVert = sf::Vertex(newVect, cellColor);
+            fanVertices.push_back(std::make_shared<sf::Vertex>(newVert));
+        }
+    }
+
+    // Draw edges
+    for(const auto& edge : edges){
+        drawEdge(*edge);
+    }
+
+    // Draw the cell
+    engine.eRenderer->addRenderCommand(fanVertices, sf::PrimitiveType::TriangleFan);
 }
 
-void VoronoiDiagram::drawEdge(const boost::polygon::voronoi_edge<double>& edge) const{
+void VoronoiDiagram::drawEdge(const boost::polygon::voronoi_edge<double>& edge, const sf::Color& color) const{
     if (edge.is_infinite()) return;
 
     auto v0 = edge.vertex0();
@@ -105,17 +171,34 @@ void VoronoiDiagram::drawEdge(const boost::polygon::voronoi_edge<double>& edge) 
     std::vector<std::shared_ptr<sf::Vertex>> line{};
     
     sf::Vertex vert1 = sf::Vertex(
-        sf::Vector2f(static_cast<float>(v0->x()), static_cast<float>(v0->y())), sf::Color::White);
+        sf::Vector2f(static_cast<float>(v0->x()), static_cast<float>(v0->y())), color);
     sf::Vertex vert2 = sf::Vertex(
-        sf::Vector2f(static_cast<float>(v1->x()), static_cast<float>(v1->y())), sf::Color::White);
+        sf::Vector2f(static_cast<float>(v1->x()), static_cast<float>(v1->y())), color);
 
     line.push_back(std::make_shared<sf::Vertex>(vert1));
     line.push_back(std::make_shared<sf::Vertex>(vert2));
 
-    engine.eRenderer->addRenderCommand(line, sf::PrimitiveType::Lines);
+    engine.eRenderer->addRenderCommand(line, sf::PrimitiveType::Lines, 101);
 }
 
-std::vector<sf::Vector2f> VoronoiDiagram::generatePoints(int amount, int range){
+void VoronoiDiagram::drawEdge(const sf::Vector2f& v0, const sf::Vector2f& v1, const sf::Color& color) const{
+    std::vector<std::shared_ptr<sf::Vertex>> line{};
+    
+    sf::Vertex vert1 = sf::Vertex(
+        v0, color);
+    sf::Vertex vert2 = sf::Vertex(
+        v1, color);
+
+    line.push_back(std::make_shared<sf::Vertex>(vert1));
+    line.push_back(std::make_shared<sf::Vertex>(vert2));
+
+    engine.eRenderer->addRenderCommand(line, sf::PrimitiveType::Lines, 101);
+}
+
+std::vector<sf::Vector2f> VoronoiDiagram::generatePoints(int amount, float range){
+    // Set bounding box
+    boundingBox = sf::Vector2f(range / 2.f, range / 2.f);
+
     // Generate random points
     std::vector<sf::Vector2f> points;
     std::mt19937 gen(std::random_device{}());
@@ -123,7 +206,7 @@ std::vector<sf::Vector2f> VoronoiDiagram::generatePoints(int amount, int range){
     std::uniform_real_distribution<float> distY;
 
     for(int i = 0; i < amount; ++i){
-        points.emplace_back(distX(gen) * range, distY(gen) * range);
+        points.emplace_back(distX(gen) * range - range / 2.f, distY(gen) * range - range / 2.f);
     }
 
     return points;
